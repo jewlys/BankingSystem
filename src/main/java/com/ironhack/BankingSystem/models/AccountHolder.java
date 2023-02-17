6280577aa9fd612e127f206111a84257f61e6e28
jewlys <jewlys@hotmail.com>
2023-02-17 11:32:17 +0100

    updating CreditCard

--- src/main/java/com/ironhack/BankingSystem/models/AccountHolder.java
+++ src/main/java/com/ironhack/BankingSystem/models/AccountHolder.java
@@ -4,31 +4,95 @@ import jakarta.persistence.*;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
-@Table(name = "account_holder")
+@Table(name = "account_holders")
 public class AccountHolder {
-
+    /* + id: Integer - Auto Inc
+    + name: String
+    + dateOfBirth: Date
+    + primaryAddress: Address
+    + mailingAddress: Address
+    + accounts: List<Account>
+    */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
-    private LocalDate dateOfBirth;
-
-    private String phoneNumber;
-
-    @Column(nullable = false, unique = true)
-    private String email;
+    private Date dateOfBirth;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id", referencedColumnName = "id")
-    private Address address;
+    private Address primaryAddress;
 
-    // Constructors, getters, and setters omitted for brevity
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address mailingAddress;
 
+    @OneToMany(mappedBy = "primaryOwner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Account> accounts = new ArrayList<>();
+
+    public AccountHolder() {
+    }
+
+    public AccountHolder(String name, Date dateOfBirth, Address primaryAddress, Address mailingAddress) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.primaryAddress = primaryAddress;
+        this.mailingAddress = mailingAddress;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Address getPrimaryAddress() {
+        return primaryAddress;
+    }
+
+    public void setPrimaryAddress(Address primaryAddress) {
+        this.primaryAddress = primaryAddress;
+    }
+
+    public Address getMailingAddress() {
+        return mailingAddress;
+    }
+
+    public void setMailingAddress(Address mailingAddress) {
+        this.mailingAddress = mailingAddress;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
 }
-
--- src/main/java/com/ironhack/BankingSystem/models/CreditCard.java
+++ src/main/java/com/ironhack/BankingSystem/models/CreditCard.java
@@ -1,13 +1,79 @@
 package com.ironhack.BankingSystem.models;
 
 import com.ironhack.BankingSystem.models.Account;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 
 import java.math.BigDecimal;
 
+@Entity
+@PrimaryKeyJoinColumn(name = "accountId")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Entity
 public class CreditCard extends Account {
+    private static final BigDecimal MAX_CREDIT_LIMIT = new BigDecimal("100000");
+    private static final BigDecimal DEFAULT_CREDIT_LIMIT = new BigDecimal("100");
+    private static final BigDecimal MIN_INTEREST_RATE = new BigDecimal("0.1");
+    private static final BigDecimal MAX_INTEREST_RATE = new BigDecimal("0.2");
     private BigDecimal creditLimit;
     private BigDecimal interestRate;
-    private BigDecimal penaltyFee;
 
-    // Getters and setters for the above fields
+    public CreditCard() {
+        super();
+        this.creditLimit = DEFAULT_CREDIT_LIMIT;
+        this.interestRate = MAX_INTEREST_RATE;
+    }
+
+    public CreditCard(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner) {
+        super(balance, primaryOwner, secondaryOwner);
+        this.creditLimit = DEFAULT_CREDIT_LIMIT;
+        this.interestRate = MAX_INTEREST_RATE;
+    }
+
+    public CreditCard(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, BigDecimal creditLimit, BigDecimal interestRate) {
+        super(balance, primaryOwner, secondaryOwner);
+        if (creditLimit.compareTo(MAX_CREDIT_LIMIT) > 0) {
+            this.creditLimit = MAX_CREDIT_LIMIT;
+        } else if (creditLimit.compareTo(DEFAULT_CREDIT_LIMIT) < 0) {
+            this.creditLimit = DEFAULT_CREDIT_LIMIT;
+        } else {
+            this.creditLimit = creditLimit;
+        }
+
+        if (interestRate.compareTo(MAX_INTEREST_RATE) > 0) {
+            this.interestRate = MAX_INTEREST_RATE;
+        } else if (interestRate.compareTo(MIN_INTEREST_RATE) < 0) {
+            this.interestRate = MIN_INTEREST_RATE;
+        } else {
+            this.interestRate = interestRate;
+        }
+    }
+
+    public BigDecimal getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(BigDecimal creditLimit) {
+        if (creditLimit.compareTo(MAX_CREDIT_LIMIT) > 0) {
+            this.creditLimit = MAX_CREDIT_LIMIT;
+        } else if (creditLimit.compareTo(DEFAULT_CREDIT_LIMIT) < 0) {
+            this.creditLimit = DEFAULT_CREDIT_LIMIT;
+        } else {
+            this.creditLimit = creditLimit;
+        }
+    }
+
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(BigDecimal interestRate) {
+        if (interestRate.compareTo(MAX_INTEREST_RATE) > 0) {
+            this.interestRate = MAX_INTEREST_RATE;
+        } else if (interestRate.compareTo(MIN_INTEREST_RATE) < 0) {
+            this.interestRate = MIN_INTEREST_RATE;
+        } else {
+            this.interestRate = interestRate;
+        }
+    }
 }
